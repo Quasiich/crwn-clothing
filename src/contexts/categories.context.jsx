072b87438c396de7f5ea7516db2ba95d55a5345f
Erik <48137656+Quasiich@users.noreply.github.com>
2024-12-03 13:00:07 +0100

    worked on directory & category shop sites

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -4,24 +4,24 @@ import {getCategoriesAndDocuments} from "../utils/firebase/firebase.utils";
 
 
 export const CategoriesContext = createContext({
-   categoriesMap: {}
+    categoriesMap: {}
 });
 
 export const CategoriesProvider = ({children}) => {
-   const [categoriesMap, setCategoriesMap] = useState()
+    const [categoriesMap, setCategoriesMap] = useState({})
 
-   useEffect( () => {
-      const getCategoriesMap = async () => {
-         const categoryMap = await getCategoriesAndDocuments();
-         console.log(categoryMap);
-         setCategoriesMap(categoryMap);
-      };
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+            setCategoriesMap(categoryMap);
+        };
 
-      getCategoriesMap();
-   }, [])
+        getCategoriesMap();
+    }, [])
 
-   const value = {categoriesMap}
-   return (
-      <CategoriesContext.Provider value={value}> {children }</CategoriesContext.Provider>
-   )
+    const value = {categoriesMap}
+    return (
+        <CategoriesContext.Provider value={value}> {children}</CategoriesContext.Provider>
+    )
 }
