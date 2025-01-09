3aee2ea9bf1bcd834771661474698c9f8281855c
Erik <48137656+Quasiich@users.noreply.github.com>
2025-01-09 12:37:21 +0100

    startet reworking cart context to redux store

--- src/index.js
+++ src/index.js
@@ -13,9 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
         <BrowserRouter>
-            <CartProvider>
-                <App/>
-            </CartProvider>
+            <App/>
         </BrowserRouter>
     </Provider>
 );
