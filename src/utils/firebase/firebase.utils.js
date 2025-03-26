24a55a6cd1361dfd3ae2e86091a61ae6929dbf85
Erik <48137656+Quasiich@users.noreply.github.com>
2025-03-26 22:38:47 +0100

    now fixed it! haha

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -22,7 +22,7 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.GOOGLE_API_KEY,
+    apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
     authDomain: "crwn-clothing-db-a4955.firebaseapp.com",
     projectId: "crwn-clothing-db-a4955",
     storageBucket: "crwn-clothing-db-a4955.firebasestorage.app",
